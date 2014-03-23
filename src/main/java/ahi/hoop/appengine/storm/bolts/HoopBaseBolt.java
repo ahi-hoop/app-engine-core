d67c18caf1bb876b411f26b0ff1c835bcfb4c43d
Paul Harvener <pharvener@yahoo.com>
2014-03-23 12:33:22 -0500

    testing git

---
+++ src/main/java/ahi/hoop/appengine/storm/bolts/HoopBaseBolt.java
@@ -0,0 +1,13 @@
+package ahi.hoop.appengine.storm.bolts;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pharvener
+ * Date: 3/23/14
+ * Time: 12:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HoopBaseBolt {
+
+    int i = 0;
+}
